76f746e19afaf4df7999e9abfc96236015cb0703
AbdalQader-Alsukari <abdalqader.alsukari@gmail.com>
2024-08-07 15:16:28 +0300

    the mul table is ready !

---
+++ mul__table/mul__table/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mul__table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int r = a * b;
+            Console.WriteLine(r);
+        }
+    }
+}
